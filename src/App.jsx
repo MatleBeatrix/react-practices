cecdbec04f2792d1d04826057df86df9341f89a7
Beatrix Mátlé <matlebeatrix@edu.bme.hu>
2022-01-07 14:20:35 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
 import './App.css';
-import Button from './Button';
-//import ButtonList from './ButtonList';
-import ButtonListWithMap from './ButtonListWithMap';
-import Profile from './Profile';
-import Profile2 from './Profile2';
-import Profile3 from './Profile3';
-import Profile4 from './Profile4';
-const buttonText = "Click Me, please!";
+import ButtonWithClass from './ButtonWithClass';
+import ButtomWithHooks from './ButtonWithHooks';
+
 
 function App() {
   return (
     <div className="App">
-      <Button text={buttonText}/>
-      {/*<ButtonList/>*/}
-      <ButtonListWithMap/>
-      <Profile/>
-      <Profile2/>
-      <Profile3/>
-      <Profile4/>
+      <ButtonWithClass/>
+      <ButtomWithHooks/>
     </div>
   );
 }
 
-/*
-function App() {
-  return (
-    <div className="App">
-      {Button(buttonText)}
-      <div id="text"></div>
-    </div>
-  );
-}
-*/
+
 
 export default App;
